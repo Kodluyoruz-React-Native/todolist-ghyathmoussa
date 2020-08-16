76635217a651254fb1159df820035bfb1d83ad10
ghyarhmoussa <ghyathmoussa@hotmail.com>
2020-08-16 23:55:38 +0300

    adding swip button

--- src/controller/anasayfaC.js
+++ src/controller/anasayfaC.js
@@ -7,7 +7,7 @@ class anasayfaC {
     spalshAktif = true;
 
     cDMount = () => { //Screen Starting
-        setTimeout(()=>this.spalshAktif = false,2000);
+        setTimeout(() => this.spalshAktif = false, 2000);
     }
 
     cDUpdate = () => { //after the updating
@@ -18,17 +18,27 @@ class anasayfaC {
 
     }
 
-    
+
+    notBtnOpenClose = 1; //for open and close the buttons
+
+    setNotButtomnOpen = i => {
+        this.notBtnOpenClose = this.notBtnOpenClose === 1 ? i : -1;
+        alert('Test')
+    }
+
+    set = (k, v) => this[k] = v;
 
 }
 
 decorate(
     anasayfaC,
     {
-        cDMount:action,
-        cDUpdate:action,
-        cDUMount:action,
+        cDMount: action,
+        cDUpdate: action,
+        cDUMount: action,
         spalshAktif: observable,
+        notBtnOpenClose: observable,
+        set: action
     }
 );
 
